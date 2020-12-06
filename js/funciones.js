56134906cc243c8c133003269393586289a8c0df
vivianacampos <vivianacampos@ugm.cl>
2020-12-06 18:05:39 -0300

    avances de hoy 6-12

--- js/funciones.js
+++ js/funciones.js
@@ -5,7 +5,13 @@ function muestra(){
 	//Aplico la clase visible al bloque span.
 	bloque.className = "visible"; 
 	//Para ocultar el link obtengo su objeto.
-	var link = document.getElementById("enlace");
+	var link = document.getElementById("enlace1");
 	link.className = "oculto";
-
 }
+function oculto(){
+	var bloque = document.getElementById("adicional");
+	bloque.className = "oculto";
+	var link = document.getElementById("enlace1");
+	link.className = "visible";
+}
+
