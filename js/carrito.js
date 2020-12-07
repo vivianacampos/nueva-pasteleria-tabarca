6023e9addb92ccbe5bd43912cd900cbddd6a0406
vivianacampos <vivianacampos@ugm.cl>
2020-12-07 18:09:38 -0300

    ultimo commit antes de la entrega

--- js/carrito.js
+++ js/carrito.js
@@ -1,10 +1,3 @@
 // JavaScript Document
 
-function anade("titulo","valor", "imagen"){
-	titulo = document.getElementById('nombre').value;
-	valor = document.getElementById('valor').value;
-	imagen = document.getElementById('imagen').value;
-	
-	
-	
-}
+
